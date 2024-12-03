a6216ce8d28d891cd733af546799f6f012f6ad3a
srinivasan18061999 <srinivasanr18061999@gmail.com>
2024-12-03 13:34:34 +0530

    day7

---
+++ NestedForloop.java
@@ -0,0 +1,25 @@
+package day7;
+
+public class NestedForloop 
+{
+	public static void main(String[] agrs)
+	{
+		for(int i=0;i>=3;i++)
+		{
+		
+			for(int j=3;j>=i;j--)
+			{
+				
+				System.out.print("*");
+				
+			}
+			System.out.println("");
+			
+			
+			
+		}
+		
+	}
+}
+
+
---
+++ VaranamAyiram.java
@@ -0,0 +1,21 @@
+package day7;
+
+import java.util.Scanner;
+
+public class VaranamAyiram
+{
+	public static void main(String[] agrs)
+	{
+		Scanner sc=new Scanner(System.in);
+		String mehana =sc.nextLine();
+		if(mehana.equals("dead"))
+		{
+		System.out.println("surya weds ramya");
+		
+		}
+		else
+		{
+			System.out.println("surya weds mehana");
+		}
+	}
+}
